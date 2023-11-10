4fcfd3807e249812289f5890c1c7e06b655039c6
“Iryna <maria14trush@gmail.com>
2023-11-10 13:55:58 -0800

    add tests

---
+++ test_server.py
@@ -0,0 +1,17 @@
+import unittest
+from server import app
+
+class FlaskTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_index_route(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 302)  
+        self.assertIn('/login', response.headers['Location']) 
+
+
+if __name__ == '__main__':
+    unittest.main()

3c8f23f54c2400e243f5ad699c02cdd7ebaf3a6e
“Iryna <maria14trush@gmail.com>
2023-11-10 14:30:39 -0800

    add tests

--- test_server.py
+++ test_server.py
@@ -23,6 +23,42 @@ class FlaskTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'You have been logged out', response.data)
 
+    def test_dashboards_route(self):
+        with self.app.session_transaction() as session:
+            session['user_id'] = 1
+        response = self.app.get('/dashboards')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'User Dashboards', response.data)
+
+    def test_create_dashboard_route(self):
+        with self.app.session_transaction() as session:
+            session['user_id'] = 1
+        response = self.app.post('/create-dashboard', data=dict(
+            title='New Dashboard',
+            language='Python'
+        ), follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'New Dashboard', response.data)
+
+    def test_delete_dashboard_route(self):
+        with self.app.session_transaction() as session:
+            session['user_id'] = 1
+        response = self.app.post('/delete-dashboard', data=dict(
+            dashboard_id=1
+        ), follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertNotIn(b'Dashboard 1', response.data)
+
+    def test_edit_dashboard_route(self):
+        with self.app.session_transaction() as session:
+            session['user_id'] = 1
+        response = self.app.post('/edit-dashboard', data=dict(
+            dashboard_id=1,
+            title='New Title'
+        ), follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'New Title', response.data)
+
 
 if __name__ == '__main__':
     unittest.main()
